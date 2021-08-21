e53fc67f7279a62869fe2b5385c1550b7d549dbf
unknown <pglpm0@gmail.com>
2021-08-21 16:55:13 +0200

    update scripts

---
+++ scripts/nonfactorizable_Rcont_6covs_v1/valuation_unfact_direct_xexch.R
@@ -0,0 +1,281 @@
+## Author: PGL  Porta Mana
+## Created: 2021-03-20T10:07:17+0100
+## Last-Updated: 2021-08-09T19:25:22+0200
+################
+## Script for evaluation of regression.
+## Unfactorizable prior
+## r|x exchangeable
+## x not exchangeable
+################
+#### Custom setup ####
+## Colour-blind friendly palettes, from https://personal.sron.nl/~pault/
+library('khroma')
+palette(colour('bright')())
+## palette(colour('muted')())
+library('data.table')
+library('ggplot2')
+library('ggthemes')
+theme_set(theme_bw(base_size=18))
+scale_colour_discrete <- scale_colour_bright
+#library('cowplot')
+library('png')
+library('foreach')
+library('doFuture')
+library('doRNG')
+registerDoFuture()
+#library('LaplacesDemon') # used for Dirichlet generator
+library('ash')
+library('extraDistr')
+library('PReMiuM')
+library('mvtnorm')
+options(bitmapType='cairo')
+
+##################################################################
+##################################################################
+## Function to calculate \sum_t F^{(t)}_{r,x}
+##
+predictYX <- function(dataobj, X){
+    dC <- setdiff(discreteCovs,'bin_RMSD')
+    X <- as.matrix(X[, c(dC,continuousCovs), with=FALSE])
+    freqs <- foreach(val=rmsdVals, .combine=rbind)%:%foreach(asample=seq_along(dataobj$nList), .combine=cbind, .inorder=FALSE)%dopar%{
+        colSums(exp(
+            log(dataobj$psiList[[asample]]) +
+            t(vapply(seq_len(dataobj$nList[asample]), function(cluster){
+                rowSums(log(
+                    vapply(dC, function(covariate){
+                        dataobj$phiList[[asample]][[covariate]][X[,covariate], cluster]
+                    }, numeric(nrow(X)))
+                )) +
+                    dmvnorm(X[,continuousCovs], mean=dataobj$muList[[asample]][continuousCovs,cluster], sigma=as.matrix(dataobj$sigmaList[[asample]][continuousCovs,continuousCovs,cluster]), log=TRUE)
+            }, numeric(nrow(X)))) +
+                    log(dataobj$phiList[[asample]][['bin_RMSD']][val,])
+        ))
+        ##     t(sapply(seq_len(dataobj$nList[asample]),function(cluster){
+        ##     exp(rowSums(log(sapply(dC, function(covariate){
+        ##         dataobj$phiList[[asample]][[covariate]][X[,covariate], cluster]
+        ##     })))) *
+        ##         dmvnorm(X[,continuousCovs], mean=dataobj$muList[[asample]][continuousCovs,cluster], sigma=as.matrix(dataobj$sigmaList[[asample]][continuousCovs,continuousCovs,cluster]))
+        ##     }))
+        ##     )
+    }
+    me <- rowMeans(freqs)
+    freqs <- cbind(means=me, stds=sqrt(rowMeans(freqs^2) - me^2))
+    dim(freqs) <- c(nrow(X), length(rmsdVals), 2)
+    #freqs[,,1] <- normalizem(freqs[,,1])
+    dimnames(freqs)[[3]] <- c('means','unimportant')
+    dimnames(freqs)[[2]] <- paste0('binRMSD_',rmsdVals)
+    freqs
+}
+## Samples of conditional probabilities for covariates, with covariance matrix
+## predictYXcross <- function(dataobj, X){
+##     X <- as.matrix(X[, c(discreteCovs,continuousCovs), with=FALSE])
+##     freqs <- foreach(asample=seq_along(dataobj$nList), .combine=cbind, .inorder=FALSE)%dopar%{
+##         colSums(exp(
+##             log(dataobj$psiList[[asample]]) +
+##             t(vapply(seq_len(dataobj$nList[asample]), function(cluster){
+##                 rowSums(log(
+##                     vapply(discreteCovs, function(covariate){
+##                         dataobj$phiList[[asample]][[covariate]][X[,covariate], cluster]
+##                     }, numeric(nrow(X)))
+##                 )) +
+##                     dmvnorm(X[,continuousCovs], mean=dataobj$muList[[asample]][continuousCovs,cluster], sigma=as.matrix(dataobj$sigmaList[[asample]][continuousCovs,continuousCovs,cluster]), log=TRUE)
+##             }, numeric(nrow(X))))
+##         ))
+##         ## colSums(dataobj$psiList[[asample]] *
+##             t(sapply(seq_len(dataobj$nList[asample]),function(cluster){
+##             exp(rowSums(log(sapply(discreteCovs, function(covariate){
+##                 dataobj$phiList[[asample]][[covariate]][X[,covariate], cluster]
+##             })))) *
+##                 dmvnorm(X[,continuousCovs], mean=dataobj$muList[[asample]][continuousCovs,cluster], sigma=as.matrix(dataobj$sigmaList[[asample]][continuousCovs,continuousCovs,cluster]))
+##             }))
+##         ##     )
+##     }
+##     me <- rowMeans(freqs)
+##     list(means=me,
+##          covariance=tcrossprod(freqs)/ncol(freqs)-tcrossprod(me))
+## }
+
+
+##
+## Utility matrices
+dgain <- diag(1,3)
+cgain <- 1-sapply(1:3,function(x){abs(x-1:3)})/2
+## function for randomly choosing ties
+resample <- function(x, ...) x[sample.int(length(x), ...)]
+## function to compute utilities in test set
+utilities <- function(truevalues,probX,utilitym){
+    if(is.null(dim(probX))){probX <- t(matrix(probX,nrow=length(probX),ncol=length(truevalues)))}
+    y <- apply(t(tcrossprod(utilitym, normalizem(probX))), 1,
+               function(z){resample(which(z==max(z)))})
+    diag(utilitym[truevalues,y])
+}
+## function to compute various prob-averages in test set
+probscores <- function(truevalues,probX,meanfunction){
+    if(is.null(dim(probX))){probX <- t(matrix(probX,nrow=length(probX),ncol=length(truevalues)))}
+    y <- normalizem(probX)
+    meanfunction(diag(y[,truevalues]))
+}
+## function to construct data table with results
+metrics <- function(truevalues, probX, chanceprior){
+    list(
+        delta_gain = data.table(
+                 model=utilities(truevalues,probX,dgain),
+                 chance=utilities(truevalues,chanceprior,dgain),
+                 range=range(dgain)
+             ),
+         ##
+        contig_gain = data.table(
+                 model=utilities(truevalues,probX,cgain),
+                 chance=utilities(truevalues,chanceprior,cgain),
+                 range=range(cgain)
+             ),
+         ##
+        log_score = data.table(
+                 model=probscores(truevalues,probX,log),
+                 chance=probscores(truevalues,chanceprior,log),
+                 range=c(-Inf,0)
+             ),
+         ##
+        mean_score = data.table(
+                 model=probscores(truevalues,probX,identity),
+                 chance=probscores(truevalues,chanceprior,identity),
+                 range=c(0,1)
+             )
+        )}
+
+##
+#### Calculation of utilities and scores for test set
+## source(file='calibration_plots.R')
+##
+case <- 1 # 'fdata'
+## Frequencies of RMSD categories in test set are same as full dataset.
+## It does not make sense to change them, because the conditional frequencies r|x
+## then also change - we can then make the model have a score as bad as we please
+##
+scoresP <- scoresU <- list(NULL,NULL)
+names(scoresP) <- names(scoresU) <- c('tail','head')
+for(dset in names(scoresP)){
+    ## Compare with a model based only on prior frequencies of r
+    priorR <- normalize(as.vector(table(data$bin_RMSD)))
+    unseldata <- setdiff(1:nrow(data), seldata)
+    nTest <- 2000
+    ## construct test set
+    testdata <- data[do.call(dset,list(x=unseldata,n=3*nTest)), covNames, with=F]
+    ## testd <- data[unseldata, covNames, with=F]
+    ## testdata <- data.table()
+    ## for(val in rmsdVals){
+    ##     testdata <- rbind(testdata, tail(testd[bin_RMSD==val],n=round(3*nTest*priorR[val])))
+    ## }
+    ## rm(testd)
+    ##
+    gc()
+    plan(sequential)
+    plan(multisession, workers = 6L)
+    condfreqs <- predictYX(MCMCdata[[case]], testdata)
+    plan(sequential)
+    ##
+    set.seed(247)
+    scoresP[[dset]] <- metrics(truevalues=testdata[,bin_RMSD], probX=condfreqs[,,1], chanceprior=priorR)
+    print(dset)
+    pdff(paste0('scores_fullexch_',dset,'_informedchance'))
+    basescores <- scoresP[[dset]]
+    ##
+print(    ggplot(dt <- melt(basescores$delta_gain[,1:2])) +
+        geom_histogram(aes(x=value, fill=variable), color='white', bins=length(unique(c(dgain))), alpha=0.5, position='identity') + scale_fill_bright() +
+        xlab('delta utility') +
+        geom_vline(data=dt[,.(value=mean(value)), by=variable], aes(xintercept=value, color=variable, linetype=variable), alpha=0.5, size=2))
+    ##
+print(    ggplot(dt <- melt(basescores$contig_gain[,1:2])) +
+        geom_histogram(aes(x=value, fill=variable), color='white', bins=length(unique(c(cgain))), alpha=0.5, position='identity') + scale_fill_bright() +
+        xlab('contiguous utility') +
+        geom_vline(data=dt[,.(value=mean(value)), by=variable], aes(xintercept=value, color=variable, linetype=variable), alpha=0.5, size=2))
+    ##
+print(    ggplot(dt <- melt(basescores$log_score[,1:2])) +
+        geom_histogram(aes(x=value, fill=variable), color='white', bins=10, alpha=0.5, position='identity') + scale_fill_bright() +
+        xlab('log-probability') +
+        geom_vline(data=dt[,.(value=mean(value)), by=variable], aes(xintercept=value, color=variable, linetype=variable), alpha=0.5, size=2))
+    ##
+print(    ggplot(dt <- melt(basescores$mean_score[,1:2])) +
+        geom_histogram(aes(x=value, fill=variable), color='white', bins=10, alpha=0.5, position='identity') + scale_fill_bright() +
+        xlab('probability') +
+        geom_vline(data=dt[,.(value=mean(value)), by=variable], aes(xintercept=value, color=variable, linetype=variable), alpha=0.5, size=2))
+    ##
+    dev.off()
+    ##
+    ## 
+    ## Compare with a "completely ignorant" model: uniform prior
+    priorU <- rep(1, length(rmsdVals))/length(rmsdVals)
+    set.seed(247)
+    scoresU[[dset]] <- metrics(truevalues=testdata[,bin_RMSD], probX=condfreqs[,,1], chanceprior=priorU)
+                                        #print(scoresU[[dset]])
+    pdff(paste0('scores_fullexch_',dset,'_uniformchance'))
+    basescores <- scoresU[[dset]]
+    ##
+print(    ggplot(dt <- melt(basescores$delta_gain[,1:2])) +
+        geom_histogram(aes(x=value, fill=variable), color='white', bins=length(unique(c(dgain))), alpha=0.5, position='identity') + scale_fill_bright() +
+        xlab('delta utility') +
+        geom_vline(data=dt[,.(value=mean(value)), by=variable], aes(xintercept=value, color=variable, linetype=variable), alpha=0.5, size=2))
+    ##
+print(    ggplot(dt <- melt(basescores$contig_gain[,1:2])) +
+        geom_histogram(aes(x=value, fill=variable), color='white', bins=length(unique(c(cgain))), alpha=0.5, position='identity') + scale_fill_bright() +
+        xlab('contiguous utility') +
+        geom_vline(data=dt[,.(value=mean(value)), by=variable], aes(xintercept=value, color=variable, linetype=variable), alpha=0.5, size=2))
+    ##
+print(    ggplot(dt <- melt(basescores$log_score[,1:2])) +
+        geom_histogram(aes(x=value, fill=variable), color='white', bins=10, alpha=0.5, position='identity') + scale_fill_bright() +
+        xlab('log-probability') +
+        geom_vline(data=dt[,.(value=mean(value)), by=variable], aes(xintercept=value, color=variable, linetype=variable), alpha=0.5, size=2))
+    ##
+print(    ggplot(dt <- melt(basescores$mean_score[,1:2])) +
+        geom_histogram(aes(x=value, fill=variable), color='white', bins=10, alpha=0.5, position='identity') + scale_fill_bright() +
+        xlab('probability') +
+        geom_vline(data=dt[,.(value=mean(value)), by=variable], aes(xintercept=value, color=variable, linetype=variable), alpha=0.5, size=2))
+    ##
+    dev.off()
+    ##
+    ##
+    }
+save(list=c('scoresP','scoresU'), file=paste0('scores_fullexch_T',nTest*3,'_direct_test_N',ndata,'_',length(covNums),'covs.RData'))
+
+
+
+stop('End')
+
+
+
+
+
+
+
+
+
+if(false){
+## TEST
+plan(multisession, workers = 6L)
+testX <- as.matrix(data[2])
+cdata <- 2
+testrx <- foreach(asample=1:length(MCMCdata[[cdata]]$nList), .combine=cbind, .inorder=F)%dopar%{
+    dC <- setdiff(discreteCovs,'bin_RMSD')
+    psis <- MCMCdata[[cdata]]$psiList[[asample]]
+    mus <- MCMCdata[[cdata]]$muList[[asample]][continuousCovs,]
+    sigmas <- MCMCdata[[cdata]]$sigmaList[[asample]][continuousCovs,continuousCovs,]
+    phis <- MCMCdata[[cdata]]$phiList[[asample]]
+    ##
+    numer <- foreach(cluster=1:length(psis), .combine=cbind)%do%{
+        psis[cluster] *
+            dmvnorm(x=testX[,continuousCovs], mean=mus[,cluster], sigma=sigmas[,,cluster], log=F) *
+            prod(foreach(dv=dC, .combine=c)%do%{ phis[[dv]][testX[,dv], cluster] }) *
+            phis[['bin_RMSD']][,cluster]
+    }
+    ##
+    denom <- foreach(cluster=1:length(psis), .combine=c)%do%{
+        psis[cluster] *
+            dmvnorm(x=testX[,continuousCovs], mean=mus[,cluster], sigma=sigmas[,,cluster], log=F) *
+            prod(foreach(dv=dC, .combine=c)%do%{ phis[[dv]][testX[,dv], cluster] })
+    }
+    ##
+    rowSums(numer)/sum(denom)
+}
+plan(sequential)
+
+}
