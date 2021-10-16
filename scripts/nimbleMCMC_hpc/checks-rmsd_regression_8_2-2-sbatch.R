28d2e028f61c8873a2023f6e8382c4e45076d9bd
unknown <pglpm0@gmail.com>
2021-10-16 19:01:47 +0200

    update scripts

--- scripts/nimbleMCMC_hpc/checks-rmsd_regression_8_2-2-sbatch.R
+++ scripts/nimbleMCMC_hpc/checks-rmsd_regression_8_2-2-sbatch.R
@@ -1,6 +1,6 @@
 ## Author: PGL  Porta Mana
 ## Created: 2021-03-20T10:07:17+0100
-## Last-Updated: 2021-10-16T17:03:58+0200
+## Last-Updated: 2021-10-16T17:44:54+0200
 ################
 ## Batch script for direct regression, continuous RMSD
 ################
@@ -13,7 +13,7 @@ baseversion <- 'checksGG12_'
 nclusters <- 2L^6
 ndata <- 2L^12 # nSamples = 37969
 niter <- 2L^10
-nstages <- 0L
+nstages <- 1L
 ncheckpoints <- 8L
 covNames <-  c('log_RMSD'
                ,'log_mcs_unbonded_polar_sasa'
