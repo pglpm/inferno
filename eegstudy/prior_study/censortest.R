7db143080b94c473e0818fd162073153a2cfbbcb
pglpm <luca@magnaspesmeretrix.org>
2022-11-29 19:55:23 +0100

    update

--- eegstudy/prior_study/censortest.R
+++ eegstudy/prior_study/censortest.R
@@ -5,20 +5,23 @@ library('nimble')
 ## real (kernel: gaussian, prior: normal + compound-gamma)
 ## categorical (kernel: categorical distr, prior: Dirichlet)
 
-includecat <- TRUE # switch to include/exclude categorical components
-ndata <- 7
-nclusters <- 4
+nvars <- 10 # switch to include/exclude categorical components
+ndata <- 500
+
+nclusters <- 64
 
 set.seed(229)
-##
-rdata <- matrix(rnorm(ndata*2, mean=0, sd=2), nrow=ndata)
-bounds <- cbind(rep(-0.5,ndata), rep(1,ndata))
-signs <- cbind(rep(-1,ndata),rep(1,ndata))
+rdata <- matrix(rnorm(ndata*nvars, mean=0, sd=2), nrow=ndata)
+## 1: 2.9, 2: 5
+bounds <- matrix(c(2.9, 5), nrow=ndata,ncol=2,byrow=T)
+xbounds <- matrix(+Inf,nrow=ndata,ncol=nvars-2)
 ## censored <- cbind(rep(1,ndata), rep(0,ndata))
-censored <- rdata>=bounds
+censored <- rdata[,1:2] >= bounds
+xcensored <- rdata >= cbind(bounds,xbounds)
 ##
 rdata2 <- rdata
-rdata[signs*rdata>=signs*bounds] <- NA
+rdata[xcensored] <- NA
+
 cbind(rdata2,rdata,censored)
 bounds[!is.na(rdata)] <- signs[!is.na(rdata)]*Inf
 
@@ -27,7 +30,7 @@ datapoints <- c(
     list(Rdata = rdata, Censored=censored*1L)
 )
 
-constants <- list(ndata=ndata, nclusters=nclusters, bounds=bounds)
+constants <- list(ndata=ndata, nclusters=nclusters, bounds=bounds, nvars=nvars)
 
 initsFunction <- function(){
     c(
@@ -37,11 +40,9 @@ initsFunction <- function(){
             alpha0 = 2,
             q=rep(1/nclusters,nclusters),
             Labels = rep(1, ndata), # all data to first cluster
-            Rrates = rinvgamma(2, shape=0.5, scale=1),
-            Rmeans = matrix(rnorm(2*nclusters, mean=0, sd=3), nrow=2),
-            Rvariances = matrix(rinvgamma(2*nclusters, shape=0.5, rate=1), nrow=2),
-            'Rdata[4, 1]'=-2,
-            'Rdata[3, 2]'=2
+            Rrates = rinvgamma(nvars, shape=0.5, scale=1),
+            Rmeans = matrix(rnorm(nvars*nclusters, mean=0, sd=3), nrow=nvars, ncol=nclusters),
+            Rvariances = matrix(rinvgamma(nvars*nclusters, shape=0.5, rate=1), nrow=nvars, ncol=nclusters)
         )
     )
 }
@@ -51,11 +52,11 @@ infmixture <- nimbleCode({
     qalpha[1:nclusters] <- 2^alpha0 * qalpha0[1:nclusters]
     q[1:nclusters] ~ ddirch(alpha=qalpha[1:nclusters])
     ##
-    for(variate in 1:2){
+    for(variate in 1:nvars){
         Rrates[variate] ~ dinvgamma(shape=0.5, scale=1)
     }
     for(cluster in 1:nclusters){
-        for(variate in 1:2){# real variates
+        for(variate in 1:nvars){# real variates
             Rmeans[variate, cluster] ~ dnorm(mean=0, var=3*3)
             Rvariances[variate, cluster] ~ dinvgamma(shape=0.5, rate=Rrates[variate])
         }
@@ -66,6 +67,8 @@ infmixture <- nimbleCode({
         ##
         for(variate in 1:2){# real variates
             Censored[datum, variate] ~ dinterval(Rdata[datum, variate], bounds[datum, variate])
+        }
+        for(variate in 1:nvars){
             Rdata[datum, variate] ~ dnorm(mean=Rmeans[variate, Labels[datum]], var=Rvariances[variate, Labels[datum]])
         }
     }
@@ -76,11 +79,12 @@ model <- nimbleModel(code=infmixture, name='model',
                      inits=initsFunction(),
                      data=datapoints,
                      dimensions=c(
-                         list(Labels=ndata,
+                         list(Censored=c(ndata,2),
+                              Labels=ndata,
                               q=nclusters,
-                              Rrates=2,
-                              Rmeans=c(2, nclusters),
-                              Rvariances=c(2, nclusters))
+                              Rrates=nvars,
+                              Rmeans=c(nvars, nclusters),
+                              Rvariances=c(nvars, nclusters))
                      )
                      )
 
@@ -88,17 +92,19 @@ Cmodel <- compileNimble(model, showCompilerOutput=FALSE)
 
 
 confmodel <- configureMCMC(Cmodel,
-                           monitors=c('Labels','q','alpha0',
-                                      'Rdata[2, 1]',
-                                      'Rdata[4, 2]',
+                           monitors=c('q','alpha0',
+                                      ## 'Labels',
+                                      ## 'Rdata[2, 1]',
+                                      ## 'Rdata[4, 2]',
                                       'Rrates', 'Rmeans', 'Rvariances'
                            ))
 
 mcsampler <- buildMCMC(confmodel)
 Cmcsampler <- compileNimble(mcsampler, resetFunctions = TRUE)
+
 ##
 Cmodel$setInits(initsFunction())
-output <- Cmcsampler$run(niter=10000, thin=1, reset=T, resetMV=TRUE, nburnin=0, time=T)
+output <- Cmcsampler$run(niter=1100, thin=1, reset=T, resetMV=TRUE, nburnin=100, time=T)
 ##
 dpsamples <- as.matrix(Cmcsampler$mvSamples)
 times <- Cmcsampler$getTimes()
