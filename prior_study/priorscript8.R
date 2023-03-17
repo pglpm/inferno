ef7e45080699e8c79eeacbb48a64cd13d7579c73
pglpm <luca@magnaspesmeretrix.org>
2023-03-17 11:33:23 +0100

    update

--- prior_study/priorscript8.R
+++ prior_study/priorscript8.R
@@ -182,6 +182,7 @@ sds <- array(sqrt(nimble::rinvgamma(2*prc*nclusters, shape=shapelow0, rate=
 plotpoints2d(nsamples=nsamples, q=q, means=means, sds=sds)
 dev.off()
 ##
+
 ##
 rowcol <- c(25,25)
 prc <- prod(rowcol)
@@ -1737,3 +1738,91 @@ tplot(x,y,type='p', pch='.', alpha=0,
       ylim=range(c(tquant(y,c(2.5,97.5)/100),xlim))
       )
 dev.off()
+
+
+
+
+#### Check resulting densities for various choices of hyperpriors - cont variate
+incl <- 95
+nsamples <- 2^12
+nclusters <- 64
+rowcol <- c(24,34)
+prc <- prod(rowcol)
+drawf <- function(n, q, means, sds){
+    ks <- sample(1:nclusters, size=n, prob=q, replace=T)
+    cbind(
+        rnorm(n,
+              mean=means[1,ks],
+              sd=sds[1,ks]
+              )
+       ,
+        rnorm(n,
+              mean=means[2,ks],
+              sd=sds[1,ks]
+              )
+    )
+}
+plotpoints2d <- function(nsamples, q, means, sds){
+    xl <-- numeric(prc)
+    ## 2D
+    for(i in 1:prc){
+        points <- drawf(n=nsamples, q=q[i,], means=means[i,,], sds=sds[i,,])
+        xl[i] <- max(abs(tquant(c(points[,1]), c((100-incl)/2,(100+incl)/2)/100)), 1)
+        yl <- max(xl[i],abs(tquant(c(points[,2]), c((100-incl)/2,(100+incl)/2)/100)), 1)
+        tplot(x=points[,1], y=points[,2], type='p', pch='.',
+              alpha=0.9,
+              xlab=NA, ylab=NA, xticks=NA, yticks=NA,
+              xlabels=NA, ylabels=NA,
+              xlim=c(-yl,yl), ylim=c(-yl,yl),
+              mar=rep(0.2,4))
+        abline(h=c(-1,1),col=alpha2hex2(0.5,2))
+        abline(v=c(-1,1),col=alpha2hex2(0.5,2))
+        abline(v=par('usr')[1:2],col='black',lwd=0.5)
+        abline(h=par('usr')[3:4],col='black',lwd=0.5)
+    }
+    xl <<- xl
+    ## 1D
+    for(i in 1:prc){
+        xgrid <- seq(-xl[i], xl[i], length.out=256)
+        ygrid <- c(sapply(1:nclusters, function(cc){
+            dnorm(xgrid, mean=means[i,1,cc], sd=sds[i,1,cc])
+            }) %*% q[i,])
+        tplot(x=xgrid, y=ygrid, lwd=1,
+              xlab=NA, ylab=NA, xticks=NA, yticks=NA,
+              xlabels=NA, ylabels=NA,
+              ylim=c(0,NA),
+              mar=rep(0.25,4))
+        abline(h=c(0),col=alpha2hex2(0.5,7))
+        abline(v=c(-1,1),col=alpha2hex2(0.5,2))
+        ## abline(v=par('usr')[1:2],col='black',lwd=0.5)
+        ## abline(h=par('usr')[3:4],col='black',lwd=0.5)
+    }
+}
+##
+set.seed(111)
+maxalpha <- 4
+alphas <- sample(rep(2^((-maxalpha):maxalpha), 2), size=prc, replace=T)
+q <- extraDistr::rdirichlet(n=prc,alpha=matrix(alphas/nclusters,nrow=prc,ncol=nclusters))
+##
+baseshape <- 1
+baseshape0 <- 1.5
+baseshape1 <- 4
+disp0 <- 1
+shapelow0 <- 2#4*5/8
+shapehigh0 <- 2#4*3/8
+rate0 <- 1 # as SD
+meansm <- rnorm(prc*2, mean=0, sd=1)
+meanss <- sqrt(nimble::rinvgamma(prc*2, shape=baseshape1, rate= nimble::rinvgamma(prc*2, shape=baseshape1, rate=1)))
+sdss <- 2^sample((-1):1, size=prc*2, replace=T)*baseshape1
+sdsr <- nimble::rinvgamma(prc*2, shape=baseshape1, rate= nimble::rinvgamma(prc*2, shape=baseshape1, rate=1))
+##
+pdff('prior2D_hyperratesB',apaper=3)
+par(mfrow=rowcol,mar = c(0,0,0,0))
+## 
+set.seed(987)
+means <- array(rnorm(2*prc*nclusters, mean=0, sd=disp0), dim=c(prc,2,nclusters))
+sds <- array(sqrt(nimble::rinvgamma(prc*2*nclusters, shape=shapelow0, rate=
+                                                           nimble::rinvgamma(prc*2*nclusters, shape=shapehigh0, rate=rate0^2))), dim=c(prc,2,nclusters))
+plotpoints2d(nsamples=nsamples, q=q, means=means, sds=sds)
+dev.off()
+##
