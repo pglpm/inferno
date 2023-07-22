e51608febe108799c2c208d2134efaf59a7ebba1
Luca <pgl@portamana.org>
2023-07-22 15:31:47 +0200

    hvl update

--- package/inferpopulation.R
+++ package/inferpopulation.R
@@ -166,7 +166,7 @@ inferpopulation <- function(data, auxmetadata, outputdir, nsamples=1200, nchains
 #### other options
     Alphatoslice <- TRUE
     Ktoslice <- TRUE
-    RWtoslice <- FALSE
+    RWtoslice <- TRUE
     ##
     ## showdata <- TRUE # 'histogram' 'scatter' FALSE TRUE
     plotmeans <- TRUE # plot frequency averages
