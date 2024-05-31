1b6b662e88ec69042f899ce2f6256bfa275741d7
pglpm <luca@magnaspesmeretrix.org>
2024-05-31 15:29:57 +0200

    update testpackage script: check for identity of results

--- tests/testpackage.R
+++ tests/testpackage.R
@@ -1,7 +1,23 @@
 source('bnpi.R')
+
 testdir <- '../tests/'
-outputdirPrefix <- paste0(paste0(testdir,'_testpackage_'), strftime(as.POSIXlt(Sys.time()), '%y%m%dT%H%M'))
-test <- inferpopulation(data=paste0(testdir,'testdata.csv'), 
-                        metadata=paste0(testdir,'metatestdata.csv'), 
-                        outputdir=outputdirPrefix, 
-                        nsamples=120, nchains=12, cleanup=F, parallel=4)
+
+refFdistributionFile <- paste0(testdir,
+                               'reference_packagetest_seed16_240531T1510-V8-D15-K64-S120/Fdistribution.rds')
+
+seed <- 16
+
+outputdirPrefix <- paste0(paste0(testdir,'__packagetest-'),
+                          strftime(as.POSIXlt(Sys.time()), '%y%m%dT%H%M'))
+
+test <- inferpopulation(data=paste0(testdir,'testdata.csv'),
+                        metadata=paste0(testdir,'metatestdata.csv'),
+                        outputdir=outputdirPrefix,
+                        nsamples=120, nchains=12, cleanup=F, parallel=4,
+                        seed=seed)
+
+currentFdistribution <- readRDS(paste0(outputdirPrefix ,
+                                       '-V8-D15-K64-S120/Fdistribution.rds'))
+
+print('Result of package test (TRUE = passed):')
+print(identical(currentFdistribution, readRDS(refFdistributionFile)))
