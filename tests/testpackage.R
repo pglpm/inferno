ffd3c05499fdeb8f0c879801c61e6b1e57e557ef
choisant <agre@hvl.no>
2024-05-31 14:53:45 +0200

    update testpackage to create folders starting with _ which are ignored

--- tests/testpackage.R
+++ tests/testpackage.R
@@ -1,3 +1,7 @@
 source('bnpi.R')
 testdir <- '../tests/'
-test <- inferpopulation(data=paste0(testdir,'testdata.csv'), metadata=paste0(testdir,'metatestdata.csv'), outputdir=paste0(paste0(testdir,'testpackage_'),strftime(as.POSIXlt(Sys.time()), '%y%m%dT%H%M')), nsamples=120, nchains=12, cleanup=F, parallel=4)
+outputdirPrefix <- paste0(paste0(testdir,'_testpackage_'), strftime(as.POSIXlt(Sys.time()), '%y%m%dT%H%M'))
+test <- inferpopulation(data=paste0(testdir,'testdata.csv'), 
+                        metadata=paste0(testdir,'metatestdata.csv'), 
+                        outputdir=outputdirPrefix, 
+                        nsamples=120, nchains=12, cleanup=F, parallel=4)
