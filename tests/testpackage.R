b636b49e57f5c853496ec7603010d938a99e99d2
pglpm <luca@magnaspesmeretrix.org>
2024-06-07 08:09:24 +0200

    update testpackages in this branch

--- tests/testpackage.R
+++ tests/testpackage.R
@@ -40,8 +40,8 @@ print(identical(
 
 #### Test whether computation log-1 is identical
 ## remove lines containing diagnostic times, as they can vary
-cat('\nVerifying equality of "_log-1.log" (TRUE = passed):\n')
-reffile <- readLines(paste0(refdir,'_log-1.log'))
+cat('\nVerifying equality of "log-1" (TRUE = passed):\n')
+reffile <- readLines(paste0(refdir,'log-1.txt'))
 reffile <- reffile[!grepl('time', reffile, fixed=TRUE)]
 currentfile <- readLines(paste0(currenttestdir,'_log-1.log'))
 currentfile <- currentfile[!grepl('time', currentfile, fixed=TRUE)]
