cc18d4d1b8664302cd53c21ab84ef96b070fa225
pglpm <pgl@portamana.org>
2025-07-02 20:46:05 +0200

    remove old files

--- development/tests/iris/iris_buildmetadata.R
+++
@@ -1,6 +0,0 @@
-data('iris')
-
-devtools::install()
-library('inferno')
-
-buildmetadata(data = iris, file = '_test_iris_metadata.csv')
--- development/tests/mtcars/mtcars_buildmetadata.R
+++
@@ -1,6 +0,0 @@
-data(mtcars)
-
-devtools::install()
-library('inferno')
-
-buildmetadata(data = mtcars, file = '_test_mtcars_metadata.csv')
