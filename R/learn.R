9b988e38c6f0d5ef2e07f6ec186081f2a1f51c58
pglpm <pgl@portamana.org>
2026-01-27 08:08:19 +0100

    update

--- R/learn.R
+++ R/learn.R
@@ -1877,8 +1877,7 @@ workerfun <- function(
                 outlist <- c(
                     outlist,
                     list(
-                        Nprob = matrix(unlist(sapply(constants$
-                                                         Ncards, function(acard){
+                        Nprob = matrix(unlist(sapply(constants$Ncards, function(acard){
                             rep(1 / acard, acard)
                         })), nrow = sum(constants$Ncards), ncol = ncomponents)
                     )
