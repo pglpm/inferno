a9e636d542879ab5f9bef8ac01ffecdcee0942d5
pglpm <pgl@portamana.org>
2026-06-19 18:51:55 +0200

    update data doc

--- R/data.R
+++ R/data.R
@@ -7,7 +7,7 @@
 #' A data frame with 2 rows and 10 columns.
 #'
 #' @seealso
-#' [metadatatemplate()] which helps producing this metadata file from a given dataset, [learn()] which uses to produce hyperparameters for posterior infereces, [learntExample] as an (unrealistic) `learnt' object produced from these metadata.
+#' [metadatatemplate()] which helps producing this metadata file from a given dataset, [learn()] which uses to produce hyperparameters for posterior infereces, [learntExample] as a `learnt' object produced from these metadata.
 "metadataExample"
 
 
@@ -16,7 +16,7 @@
 #' @description
 #' An example `learnt` object obtained by means of the [learn()] function, using the [datasets::penguins] dataset and the metadata in [metadataExample]. It is a list that essentially contains posterior hyperparameters for drawing statistical inferences about the variates `species` and `bill_len`.
 #'
-#' **Note** that the [learn()] function that produced `learntExample` was called with special, poor parameter values to reduce its computation time and memory size, at the cost of Monte Carlo convergence. Thus no real inferential or statistical meaning should be attached to probabilities obtained from `learntExample`. It is only used for illustration purposes of the package's capabilities.
+#' **Note** that the [learn()] function that produced `learntExample` was called with the option to create only a limited number (360) of Monte Carlo samples, in order to reduce its memory size. Thus the numerical error associated with the Monte Carlo approximation is relatively in inferences drawn from the posterior hyperparameters saved in `learntExample`. It is only meant to be used for illustration purposes of the package's capabilities.
 #'
 #' @format ## `learntExample`
 #' A list containing results from Markov-chain Monte Carlo computation, including diagnostics and variate metadata.
