0f969ef5b21b7fd7d08a310c1e2666d84f3f809d
pglpm <pgl@portamana.org>
2026-06-22 22:55:09 +0200

    add print() and remove subset()

--- R/util_prsubset.R
+++ R/util_prsubset.R
@@ -7,43 +7,43 @@
 #'
 #' @return A list of class `probability`, identical to the original object `x` except for a reduced range of values in some if its variates.
 #'
-#' @seealso
-#' [Pr()], which generates probability objects.
-#'
-#' [plot.probability()] to plot probabilities and quantiles calculated by `Pr()'.
-#'
-#' [hist.probability()] to plot histograms of the probability distributions calculated by `Pr()`.
-#'
-#' @examples
-#' ## Load the example `learnt` object calculated from the "penguins" dataset;
-#' ## variates: 'species' and 'bill_len'
-#' learnt <- learntExample
-#'
-#' ## Calculate the probability object for the three values of variate 'species',
-#' ## given values 43 and 44 of variate 'bill_len';
-#' ## this object contains probabilities, quantiles, and other information
-#' probs <- Pr(
-#'   Y = data.frame(species = c('Adelie', 'Chinstrap', 'Gentoo')),
-#'   X = data.frame(bill_len = c(43, 44)),
-#'   learnt = learnt, parallel = 1
-#' )
-#'
-#' probs$values
-#'
-#' ## Subset by retaining the values 'Adelie' and 'Gentoo' for species,
-#' ## and 44 for bill length
-#' newprobs <- prsubset(
-#'   probs,
-#'   subset = list(species = c('Adelie', 'Gentoo'), bill_len = 43)
-#' )
-#'
-#' newprobs$values
-#'
-#' ## Plot these conditional probabilities and their variabilities
-#' plot(newprobs)
-#'
-#' hist(newprobs)
-#'
+## #' @seealso
+## #' [Pr()], which generates probability objects.
+## #'
+## #' [plot.probability()] to plot probabilities and quantiles calculated by `Pr()'.
+## #'
+## #' [hist.probability()] to plot histograms of the probability distributions calculated by `Pr()`.
+## #'
+## #' @examples
+## #' ## Load the example `learnt` object calculated from the "penguins" dataset;
+## #' ## variates: 'species' and 'bill_len'
+## #' learnt <- learntExample
+## #'
+## #' ## Calculate the probability object for the three values of variate 'species',
+## #' ## given values 43 and 44 of variate 'bill_len';
+## #' ## this object contains probabilities, quantiles, and other information
+## #' probs <- Pr(
+## #'   Y = data.frame(species = c('Adelie', 'Chinstrap', 'Gentoo')),
+## #'   X = data.frame(bill_len = c(43, 44)),
+## #'   learnt = learnt, parallel = 1
+## #' )
+## #'
+## #' probs$values
+## #'
+## #' ## Subset by retaining the values 'Adelie' and 'Gentoo' for species,
+## #' ## and 44 for bill length
+## #' newprobs <- prsubset(
+## #'   probs,
+## #'   subset = list(species = c('Adelie', 'Gentoo'), bill_len = 43)
+## #' )
+## #'
+## #' newprobs$values
+## #'
+## #' ## Plot these conditional probabilities and their variabilities
+## #' plot(newprobs)
+## #'
+## #' hist(newprobs)
+## #'
 #' @keywords internal
 prsubset <- function(
     x,
